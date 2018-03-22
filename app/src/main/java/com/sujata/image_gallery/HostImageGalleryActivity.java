cb672e10da11997d818eef36f735aa10aeee0d40
Jana <janardhan.srit@gmail.com>
2018-03-22 15:06:54 +0530

    Initial commit

---
+++ app/src/main/java/com/sujata/image_gallery/HostImageGalleryActivity.java
@@ -0,0 +1,48 @@
+package com.sujata.image_gallery;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.etiennelawlor.imagegallery.library.ImageGalleryFragment;
+
+import butterknife.ButterKnife;
+
+public class HostImageGalleryActivity extends AppCompatActivity {
+
+    // region Lifecycle Methods
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_image_gallery);
+        ButterKnife.bind(this);
+
+        Fragment fragment = getSupportFragmentManager().findFragmentById(android.R.id.content);
+        if (fragment == null) {
+            fragment = ImageGalleryFragment.newInstance(getIntent().getExtras());
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(android.R.id.content, fragment, "")
+                    .commit();
+        } else {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .attach(fragment)
+                    .commit();
+        }
+    }
+    // endregion
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                supportFinishAfterTransition();
+//                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/sujata/image_gallery/MainActivity.java
@@ -0,0 +1,288 @@
+package com.sujata.image_gallery;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.graphics.Palette;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.etiennelawlor.imagegallery.library.ImageGalleryFragment;
+import com.etiennelawlor.imagegallery.library.activities.FullScreenImageGalleryActivity;
+import com.etiennelawlor.imagegallery.library.activities.ImageGalleryActivity;
+import com.etiennelawlor.imagegallery.library.adapters.FullScreenImageGalleryAdapter;
+import com.etiennelawlor.imagegallery.library.adapters.ImageGalleryAdapter;
+import com.etiennelawlor.imagegallery.library.enums.PaletteColorType;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Callback;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements ImageGalleryAdapter.ImageThumbnailLoader, FullScreenImageGalleryAdapter.FullScreenImageLoader {
+
+    // region Member Variables
+    private PaletteColorType paletteColorType;
+    // endregion
+
+  /*  // region Listeners
+    @OnClick(R.id.image_gallery_activity_btn)
+    public void onImageGalleryActivityButtonClicked() {
+        Intent intent = new Intent(MainActivity.this, HostImageGalleryActivity.class);
+
+        String[] images = getResources().getStringArray(R.array.unsplash_images);
+        Bundle bundle = new Bundle();
+        bundle.putStringArrayList(ImageGalleryActivity.KEY_IMAGES, new ArrayList<>(Arrays.asList(images)));
+        Log.e("testing", "array========");
+        bundle.putString(ImageGalleryActivity.KEY_TITLE, "Unsplash Images");
+        Log.e("testing", "array");
+        intent.putExtras(bundle);
+
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.image_gallery_fragment_btn)
+    public void onImageGalleryFragmentButtonClicked() {
+        Intent intent = new Intent(MainActivity.this, HostImageGalleryActivity.class);
+
+        String[] images = getResources().getStringArray(R.array.unsplash_images);
+        Bundle bundle = new Bundle();
+        bundle.putStringArrayList(ImageGalleryFragment.KEY_IMAGES, new ArrayList<>(Arrays.asList(images)));
+        bundle.putString(ImageGalleryActivity.KEY_TITLE, "Unsplash Images");
+        intent.putExtras(bundle);
+
+        startActivity(intent);
+    }*/
+    // endregion
+    Button button,button1;
+
+    // region Lifecycle Methods
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        button=(Button)findViewById(R.id.image_gallery_activity_btn);
+        button1=(Button)findViewById(R.id.image_gallery_fragment_btn);
+        ImageGalleryActivity.setImageThumbnailLoader(this);
+        ImageGalleryFragment.setImageThumbnailLoader(this);
+        FullScreenImageGalleryActivity.setFullScreenImageLoader(this);
+
+        // optionally set background color using Palette for full screen images
+        paletteColorType = PaletteColorType.VIBRANT;
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HostImageGalleryActivity.class);
+
+                String[] images = getResources().getStringArray(R.array.unsplash_images);
+                Bundle bundle = new Bundle();
+                bundle.putStringArrayList(ImageGalleryActivity.KEY_IMAGES, new ArrayList<>(Arrays.asList(images)));
+                Log.e("testing", "array========");
+                bundle.putString(ImageGalleryActivity.KEY_TITLE, "Unsplash Images");
+                Log.e("testing", "array");
+                intent.putExtras(bundle);
+
+                startActivity(intent);
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HostImageGalleryActivity.class);
+
+                String[] images = getResources().getStringArray(R.array.unsplash_images);
+                Bundle bundle = new Bundle();
+                bundle.putStringArrayList(ImageGalleryFragment.KEY_IMAGES, new ArrayList<>(Arrays.asList(images)));
+                bundle.putString(ImageGalleryActivity.KEY_TITLE, "Unsplash Images");
+                intent.putExtras(bundle);
+
+                startActivity(intent);
+            }
+        });
+    }
+    // endregion
+
+    // region ImageGalleryAdapter.ImageThumbnailLoader Methods
+    @Override
+    public void loadImageThumbnail(final ImageView iv, String imageUrl, int dimension) {
+        if (!TextUtils.isEmpty(imageUrl)) {
+            Picasso.with(iv.getContext())
+                    .load(imageUrl)
+                    .resize(dimension, dimension)
+                    .centerCrop()
+                    .into(iv);
+        } else {
+            iv.setImageDrawable(null);
+        }
+    }
+    // endregion
+
+    // region FullScreenImageGalleryAdapter.FullScreenImageLoader
+    @Override
+    public void loadFullScreenImage(final ImageView iv, String imageUrl, int width, final LinearLayout bgLinearLayout) {
+        if (!TextUtils.isEmpty(imageUrl)) {
+            Picasso.with(iv.getContext())
+                    .load(imageUrl)
+                    .resize(width, 0)
+                    .into(iv, new Callback() {
+                        @Override
+                        public void onSuccess() {
+                            Bitmap bitmap = ((BitmapDrawable) iv.getDrawable()).getBitmap();
+                            Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                                public void onGenerated(Palette palette) {
+                                    applyPalette(palette, bgLinearLayout);
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onError() {
+
+                        }
+                    });
+        } else {
+            iv.setImageDrawable(null);
+        }
+    }
+    // endregion
+
+    // region Helper Methods
+    private void applyPalette(Palette palette, ViewGroup viewGroup){
+        int bgColor = getBackgroundColor(palette);
+        if (bgColor != -1)
+            viewGroup.setBackgroundColor(bgColor);
+    }
+
+    private void applyPalette(Palette palette, View view){
+        int bgColor = getBackgroundColor(palette);
+        if (bgColor != -1)
+            view.setBackgroundColor(bgColor);
+    }
+
+    private int getBackgroundColor(Palette palette) {
+        int bgColor = -1;
+
+        int vibrantColor = palette.getVibrantColor(0x000000);
+        int lightVibrantColor = palette.getLightVibrantColor(0x000000);
+        int darkVibrantColor = palette.getDarkVibrantColor(0x000000);
+
+        int mutedColor = palette.getMutedColor(0x000000);
+        int lightMutedColor = palette.getLightMutedColor(0x000000);
+        int darkMutedColor = palette.getDarkMutedColor(0x000000);
+
+        if (paletteColorType != null) {
+            switch (paletteColorType) {
+                case VIBRANT:
+                    if (vibrantColor != 0) { // primary option
+                        bgColor = vibrantColor;
+                    } else if (lightVibrantColor != 0) { // fallback options
+                        bgColor = lightVibrantColor;
+                    } else if (darkVibrantColor != 0) {
+                        bgColor = darkVibrantColor;
+                    } else if (mutedColor != 0) {
+                        bgColor = mutedColor;
+                    } else if (lightMutedColor != 0) {
+                        bgColor = lightMutedColor;
+                    } else if (darkMutedColor != 0) {
+                        bgColor = darkMutedColor;
+                    }
+                    break;
+                case LIGHT_VIBRANT:
+                    if (lightVibrantColor != 0) { // primary option
+                        bgColor = lightVibrantColor;
+                    } else if (vibrantColor != 0) { // fallback options
+                        bgColor = vibrantColor;
+                    } else if (darkVibrantColor != 0) {
+                        bgColor = darkVibrantColor;
+                    } else if (mutedColor != 0) {
+                        bgColor = mutedColor;
+                    } else if (lightMutedColor != 0) {
+                        bgColor = lightMutedColor;
+                    } else if (darkMutedColor != 0) {
+                        bgColor = darkMutedColor;
+                    }
+                    break;
+                case DARK_VIBRANT:
+                    if (darkVibrantColor != 0) { // primary option
+                        bgColor = darkVibrantColor;
+                    } else if (vibrantColor != 0) { // fallback options
+                        bgColor = vibrantColor;
+                    } else if (lightVibrantColor != 0) {
+                        bgColor = lightVibrantColor;
+                    } else if (mutedColor != 0) {
+                        bgColor = mutedColor;
+                    } else if (lightMutedColor != 0) {
+                        bgColor = lightMutedColor;
+                    } else if (darkMutedColor != 0) {
+                        bgColor = darkMutedColor;
+                    }
+                    break;
+                case MUTED:
+                    if (mutedColor != 0) { // primary option
+                        bgColor = mutedColor;
+                    } else if (lightMutedColor != 0) { // fallback options
+                        bgColor = lightMutedColor;
+                    } else if (darkMutedColor != 0) {
+                        bgColor = darkMutedColor;
+                    } else if (vibrantColor != 0) {
+                        bgColor = vibrantColor;
+                    } else if (lightVibrantColor != 0) {
+                        bgColor = lightVibrantColor;
+                    } else if (darkVibrantColor != 0) {
+                        bgColor = darkVibrantColor;
+                    }
+                    break;
+                case LIGHT_MUTED:
+                    if (lightMutedColor != 0) { // primary option
+                        bgColor = lightMutedColor;
+                    } else if (mutedColor != 0) { // fallback options
+                        bgColor = mutedColor;
+                    } else if (darkMutedColor != 0) {
+                        bgColor = darkMutedColor;
+                    } else if (vibrantColor != 0) {
+                        bgColor = vibrantColor;
+                    } else if (lightVibrantColor != 0) {
+                        bgColor = lightVibrantColor;
+                    } else if (darkVibrantColor != 0) {
+                        bgColor = darkVibrantColor;
+                    }
+                    break;
+                case DARK_MUTED:
+                    if (darkMutedColor != 0) { // primary option
+                        bgColor = darkMutedColor;
+                    } else if (mutedColor != 0) { // fallback options
+                        bgColor = mutedColor;
+                    } else if (lightMutedColor != 0) {
+                        bgColor = lightMutedColor;
+                    } else if (vibrantColor != 0) {
+                        bgColor = vibrantColor;
+                    } else if (lightVibrantColor != 0) {
+                        bgColor = lightVibrantColor;
+                    } else if (darkVibrantColor != 0) {
+                        bgColor = darkVibrantColor;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return bgColor;
+    }
+    // endregion
+}
